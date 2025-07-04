991d83beb29a89e4dcd39aff9f3adde795315b31
abnjv <abwbkralsrajy53@gmail.com>
2025-07-04 14:54:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -26,4 +26,6 @@ io.on("connection", socket => {
   });
 });
 
-server.listen(3000, () => console.log("Server is running"));
+// ✅ التعديل هنا:
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
