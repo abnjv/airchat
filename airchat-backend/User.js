b9d2cda2e074511b5a5334b3e85c1e062a416d77
abnjv <abwbkralsrajy53@gmail.com>
2025-07-17 18:39:20 +0300

    Update User.js

--- airchat-backend/User.js
+++ airchat-backend/User.js
@@ -1,10 +1,26 @@
-
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
 }, { timestamps: true });
+
+// دالة لفحص كلمة المرور عند تسجيل الدخول
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// تشفير كلمة المرور قبل الحفظ
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
 
 module.exports = mongoose.model('User', UserSchema);
